001b99e24c3eace00bbed56eadd3f734379313ba
yaokai <yaokai729@outlook.com>
2019-07-26 22:58:39 +0800

    resume start

---
+++ src/components/StyleEditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="styleEditor" ref="container">
+    <div class="code" v-html="codeInStyleTag">hah</div>
+    <pre class v-html="highlightedCode"></pre>
+  </div>
+</template>
+
+<script>
+import Prism from "prismjs";
+export default {
+  name: "styleEditor",
+  props: ["code"],
+  computed: {
+    codeInStyleTag: function() {
+      return `<style>${this.code}</style>`;
+    },
+    // 使用prismjs实现代码高亮
+    highlightedCode: function() {
+      return Prism.highlight(this.code, Prism.languages.css);
+    }
+  },
+  methods: {
+    // 实现向下自动滚动
+    goBottom() {
+      this.$refs.container.scrollTop = 100000;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+pre {
+}
+@media (max-width: 500px) {
+  pre {
+  }
+}
+.code {
+  display: none;
+}
+</style>
